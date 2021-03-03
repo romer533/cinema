72c90b9af44983e7947834137e77fdd8d0f7bf27
RomanGoltsov <romer533@mail.ru>
2021-03-03 17:41:59 +0600

    Completed Cinema with modules

--- src/main/scala/Cinema.scala
+++ src/main/scala/Cinema.scala
@@ -4,31 +4,43 @@ import akka.actor.{ActorSystem, Props}
 import akka.persistence._
 
 case class BuyPlace(placeId: Int)
+
 case class ReturnPlace(placeId: Int)
+
 case class BuyPillow(placeId: Int)
+
 case class BuyPlaid(placeId: Int)
+
 case class BuyMattress(placeId: Int)
 
 sealed trait BookState
-case class Free() extends BookState
-case class Booked(item: List[String] = List("Pillow", "Plaid", "Mattress")) extends BookState
+
+case class IsFree() extends BookState
+
+case class IsBooked(items: List[String] = Nil) extends BookState
 
 sealed trait CinemaRoomEvent
+
 object CinemaRoomEvent {
 
-  case class Booked(placeId: Int, value: Int) extends CinemaRoomEvent
-  case class Unbooked(placeId: Int, value: Int) extends CinemaRoomEvent
-  case class AddedItem(placeId: Int, item:  Int) extends CinemaRoomEvent
+  case class Booked(placeId: Int) extends CinemaRoomEvent
+
+  case class Unbooked(placeId: Int) extends CinemaRoomEvent
+
+  case class AddedItem(placeId: Int, item: String) extends CinemaRoomEvent
 
   def applyEvent(currentState: State, event: CinemaRoomEvent): State = event match {
-    case Booked(placeId, value) => State(currentState.places + (placeId -> Map(value -> true)))
-    case Unbooked(placeId, value) => State(currentState.places + (placeId -> Map(value -> false)))
-    case AddedItem(placeId, item) => State(currentState.places + (placeId -> Map(item -> true)))
+    case Booked(placeId) => State(currentState.places.patch(placeId, List(IsBooked()), 1))
+    case Unbooked(placeId) => State(currentState.places.patch(placeId, List(IsFree()), 1)) //State(currentState.places + (placeId -> Map(value -> false)))
+    case AddedItem(placeId, item) => State(currentState.places.map {
+      case IsFree() => IsFree()
+      case IsBooked(items) => IsBooked(items.patch(placeId, List(item), 0))
+    })
   }
 
 }
 
-case class State(places: Map[Int, Map[Int, Boolean]] = Map(0 -> Map(0 -> false))) {
+case class State(places: List[BookState]) {
 
   def update(event: CinemaRoomEvent): State = event match {
     case booked: Booked => applyEvent(copy(places), booked)
@@ -38,10 +50,10 @@ case class State(places: Map[Int, Map[Int, Boolean]] = Map(0 -> Map(0 -> false))
 
 }
 
-class CinemaActor extends PersistentActor {
-  override def persistenceId: String = "cinema-id-2"
+class CinemaActor(defaultState: State) extends PersistentActor {
+  override def persistenceId: String = "cinema-id-3"
 
-  var state: State = State()
+  var state: State = defaultState
 
   def updateState(event: CinemaRoomEvent): Unit =
     state = state.update(event)
@@ -54,57 +66,63 @@ class CinemaActor extends PersistentActor {
   val snapshotInterval = 100
 
   override def receiveCommand: Receive = {
-    case BuyPlace(placeId) if (state.places get placeId) == Some(Map(0 -> false)) =>
+    case BuyPlace(placeId) if state.places(placeId) == IsFree() =>
       println(s"Place $placeId is book")
-      persist(Booked(placeId, 0)) { event =>
+      persist(Booked(placeId)) { event =>
         updateState(event)
         context.system.eventStream.publish(event)
         if (lastSequenceNr % snapshotInterval == 0 && lastSequenceNr != 0) saveSnapshot(state)
       }
-    case ReturnPlace(placeId) if state.places.contains(placeId) =>
+    case ReturnPlace(placeId) =>
       println(s"Place $placeId is free")
-      persist(Unbooked(placeId, 0)) { event =>
+      persist(Unbooked(placeId)) { event =>
         updateState(event)
         context.system.eventStream.publish(event)
         if (lastSequenceNr % snapshotInterval == 0 && lastSequenceNr != 0) saveSnapshot(state)
       }
-    case BuyPillow(placeId) =>
+    case BuyPillow(placeId) if {
+      state.places(placeId) != IsFree() &&
+      state.places(placeId) != IsBooked(List("pillow"))
+    } =>
       println(s"Buy pillow for $placeId place")
-      persist(AddedItem(placeId, 1)) { event =>
+      persist(AddedItem(placeId, "pillow")) { event =>
         updateState(event)
         context.system.eventStream.publish(event)
         if (lastSequenceNr % snapshotInterval == 0 && lastSequenceNr != 0) saveSnapshot(state)
       }
-    case BuyPlaid(placeId) =>
+    case BuyPlaid(placeId) if {
+      state.places(placeId) != IsFree() &&
+      state.places(placeId) != IsBooked(List("plaid"))
+    } =>
       println(s"Buy plaid for $placeId place")
-      persist(AddedItem(placeId, 2)) { event =>
+      persist(AddedItem(placeId, "plaid")) { event =>
         updateState(event)
         context.system.eventStream.publish(event)
         if (lastSequenceNr % snapshotInterval == 0 && lastSequenceNr != 0) saveSnapshot(state)
       }
-    case BuyMattress(placeId) =>
+    case BuyMattress(placeId) if
+      state.places(placeId) == IsBooked(List("pillow", "plaid")) ||
+      state.places(placeId) == IsBooked(List("plaid", "pillow")) =>
       println(s"Buy mattress for $placeId place")
-      persist(AddedItem(placeId, 3)) { event =>
+      persist(AddedItem(placeId, "mattress")) { event =>
         updateState(event)
         context.system.eventStream.publish(event)
+        if (lastSequenceNr % snapshotInterval == 0 && lastSequenceNr != 0) saveSnapshot(state)
       }
-    case _ => println("Some message")
+    case _ => println(s"Some message || ${state.places(12)}")
   }
 }
 
 object Cinema extends App {
 
   val system = ActorSystem("cinema")
-  val cinemaActor = system.actorOf(Props[CinemaActor](), "cinema-1-zone")
+  val size = 100
+  val defaultState = State(List.fill(size)(IsFree()))
+  val cinemaActor = system.actorOf(Props(new CinemaActor(defaultState)), name = "cinema-1-zone")
 
-  cinemaActor ! BuyPlace(1)
   cinemaActor ! BuyPlace(2)
-  cinemaActor ! BuyPlace(3)
-  cinemaActor ! BuyPlace(4)
-  cinemaActor ! ReturnPlace(1)
-  cinemaActor ! BuyPlace(1)
 
-  Thread.sleep(5000)
+  Thread.sleep(1000)
   system.terminate()
 
 }
